14c5eb1e712f2e9abf703521f798029a75c02532
Mohamed <mohammedmohamuud.01@gmail.com>
2024-04-01 17:31:17 +0300

    Added error handling middleware

--- Server/Server.js
+++ Server/Server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 require("dotenv").config();
 const userRoutes = require("./Src/Routes/userRoutes");
+const errorHandler = require("./Src/Middlewares/errorHandler");
 const PORT = process.env.PORT;
 
 const Server = express();
 
 Server.use(express.json());
 Server.use(express.urlencoded({ extended: true }));
-
 Server.use("/api/", userRoutes);
+
+Server.use(errorHandler);
 
 Server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
