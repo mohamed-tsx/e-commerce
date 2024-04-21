70434f8754f26dcb940a5854810083b393530358
Mohamed <mohammedmohamuud.01@gmail.com>
2024-04-21 22:03:53 +0300

    some major fixes

--- Server/Src/Config/generateToken.js
+++ Server/Src/Config/generateToken.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const generateToken = (id, email) => {
+  // Fetch the id and email
   const payload = { id, email };
   const secret = process.env.JWT_SECRET;
   return jwt.sign(payload, secret, {
