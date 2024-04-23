40be31ea4f4dcec6215584485250ab46c9c0fa3a
Mohamed <mohammedmohamuud.01@gmail.com>
2024-04-23 22:41:17 +0300

    Added the logo and favivon also made item counter for the shopping bag icon

--- Client/src/Components/Header.tsx
+++ Client/src/Components/Header.tsx
@@ -1,11 +1,32 @@
+import { AiOutlineShopping } from "react-icons/ai";
+import Logo from "/Zylo Logo.png";
+import { useState } from "react";
 const Header = () => {
+  const [itemCount, setItemCount] = useState(10);
   return (
     <div className="mt-4">
       <ul className="flex justify-between items-center">
-        <li>Logo</li>
-        <div className="flex gap-3">
+        <img src={Logo} width={50} alt="" />
+        <div className="flex gap-3 items-center">
           <li>Home</li>
-          <li>Cart</li>
+          <div className="relative">
+            <AiOutlineShopping size={26} />
+            {itemCount > 0 && itemCount > 9 ? (
+              <div
+                className="absolute top-0 right-0 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-white text-xs font-bold"
+                style={{ transform: "translate(50%, -50%)" }}
+              >
+                9+
+              </div>
+            ) : (
+              <div
+                className="absolute top-0 right-0 bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-white text-xs font-bold"
+                style={{ transform: "translate(50%, -50%)" }}
+              >
+                {itemCount}
+              </div>
+            )}
+          </div>
         </div>
         <div className="flex items-center gap-4">
           <button className="p-1 px-4 text-center bg-black text-white rounded-full">
