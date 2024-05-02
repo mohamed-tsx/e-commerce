4c6a37b8a37dc33ab8e4c535f9bdd9618a69f5de
Mohamed <mohammedmohamuud.01@gmail.com>
2024-05-02 17:27:49 +0300

    Added the photo that i'll use for the hero

--- Client/src/Components/Hero.tsx
+++ Client/src/Components/Hero.tsx
@@ -1,17 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
     <div className="flex gap-2 w-1/2">
       <div className="mt-56">
-        <h1 className="font-semibold text-5xl">Zylo | Style Center</h1>
+        <h1 className="font-semibold text-5xl">
+          Zylo <span className=" rounded-md bg-black text-white">|</span> Style
+          Centeral
+        </h1>
         <p className="font-medium mt-6 text-2xl">
           Welcome to Zylo, where fashion meets comfort. Explore our curated
           collection of clothing, accessories, and more.
         </p>
+        <button className="flex bg-black px-16 rounded-md p-2 text-white mt-6">
+          <Link to={"/"}>Shop now</Link>
+        </button>
       </div>
       <div className="mt-56">
-        <div>
-            
-        </div>
+        <div></div>
       </div>
     </div>
   );
