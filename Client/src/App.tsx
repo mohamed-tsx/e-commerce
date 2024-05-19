ad5c0f68509bc65f677017d8f1956bae5c80544f
Mohamed <mohammedmohamuud.01@gmail.com>
2024-05-19 21:26:00 +0300

    Linked cart page with the corresponding route

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,13 +4,14 @@ import Home from "./Pages/Home Page/Home";
 import SignUp from "./Pages/Auth Pages/SignUp";
 import SignIn from "./Pages/Auth Pages/SignIn";
 import Shop from "./Pages/Shop Page/Shop";
+import Cart from "./Pages/Cart Page/Cart";
 export default function App() {
   return (
     <div className="px-16 py-6">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<h1>Hi</h1>} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/profile" element={<h1>Profile Page</h1>} />
