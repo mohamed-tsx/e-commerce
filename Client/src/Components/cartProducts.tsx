c4cee2593e9321eabc55ed030313cce019906348
Mohamed <mohammedmohamuud.01@gmail.com>
2024-05-30 20:36:32 +0300

    I fixed a bug which was causing the checkout and total to come back every time map happens

--- Client/src/Components/cartProducts.tsx
+++ Client/src/Components/cartProducts.tsx
@@ -6,50 +6,47 @@ import { useNavigate } from "react-router-dom";
 const CartProducts = () => {
   const cartProducts = useCart((state) => state.Products);
   const clearAll = useCart((state) => state.removeAllProducts);
-  var total = 0;
   const navigate = useNavigate();
+  const total = useCart((state) => state.totalPrice);
 
   return (
     <div>
       {cartProducts.length > 0 ? (
-        cartProducts.map(
-          (cartProduct) => (
-            (total += cartProduct.productPrice),
-            (
-              <div key={cartProduct.id}>
-                <CartProduct {...cartProduct} />
-                <div className="flex justify-between mt-10 items-center">
-                  <div>
-                    <button
-                      onClick={() => clearAll()}
-                      className="border py-2 px-10 border-black rounded-md"
-                    >
-                      Clear All
-                    </button>
-                  </div>
-                  <div className="flex flex-col">
-                    <div className="flex items-center justify-between gap-14">
-                      <h1 className="font-medium text-lg">Subtotal: </h1>
-                      <h1 className="font-medium text-lg">${total}</h1>
-                    </div>
-                    <div className="flex flex-col">
-                      <p>Taxes and pay are included and alot</p>
-                      <button className="bg-blue-600 text-white p-2 rounded-md mt-2">
-                        Checkout
-                      </button>
-                      <button
-                        onClick={() => navigate("/shop")}
-                        className="mt-3 flex items-center justify-end gap-3"
-                      >
-                        <BiArrowBack /> Continue Shopping
-                      </button>
-                    </div>
-                  </div>
-                </div>
+        <div>
+          {cartProducts.map((cartProduct) => (
+            <div key={cartProduct.id}>
+              <CartProduct {...cartProduct} />
+            </div>
+          ))}
+          <div className="flex justify-between mt-10 items-center">
+            <div>
+              <button
+                onClick={() => clearAll()}
+                className="border py-2 px-10 border-black rounded-md"
+              >
+                Clear All
+              </button>
+            </div>
+            <div className="flex flex-col">
+              <div className="flex items-center justify-between gap-14">
+                <h1 className="font-medium text-lg">Subtotal: </h1>
+                <h1 className="font-medium text-lg">${total}</h1>
               </div>
-            )
-          )
-        )
+              <div className="flex flex-col">
+                <p>Taxes and pay are included and alot</p>
+                <button className="bg-blue-600 text-white p-2 rounded-md mt-2">
+                  Checkout
+                </button>
+                <button
+                  onClick={() => navigate("/shop")}
+                  className="mt-3 flex items-center justify-end gap-3"
+                >
+                  <BiArrowBack /> Continue Shopping
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       ) : (
         <div className="flex flex-col items-center justify-center mt-12">
           <p className="">There's no products in your cart!</p>
