d3c0095e5b184b068e38f0cd1fa53da9577e6d4c
Mohamed <mohammedmohamuud.01@gmail.com>
2024-06-02 17:13:29 +0300

    Solved a bug which was when product got incremented it can go -k so i made it gets removed when it reaches 0

--- Client/src/Hooks/useCart.ts
+++ Client/src/Hooks/useCart.ts
@@ -30,19 +30,45 @@ export const useCart = create<UseCart>()(
       totalItems: 0,
       addProduct: (product: Product) => {
         const cart = get().Products;
-        set((state) => ({
-          Products: [...cart, { ...product, quantity: 1 }],
-          totalItems: state.totalItems + 1,
-          totalPrice: state.totalPrice + product.productPrice,
-        }));
+        const cartItem = cart.find((item) => item.id === product.id);
+        if (cartItem) {
+          // If product already in cart, increase its quantity
+          set((state) => {
+            const updatedCart = state.Products.map((item) =>
+              item.id === product.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            );
+            return {
+              Products: updatedCart,
+              totalItems: state.totalItems + 1,
+              totalPrice: state.totalPrice + product.productPrice,
+            };
+          });
+        } else {
+          // If product not in cart, add it with quantity 1
+          set((state) => ({
+            Products: [...cart, { ...product, quantity: 1 }],
+            totalItems: state.totalItems + 1,
+            totalPrice: state.totalPrice + product.productPrice,
+          }));
+        }
       },
       removeProduct: (product: Product) => {
-        set((state) => ({
-          Products: state.Products.filter((item) => item.id !== product.id),
-          totalItems: state.totalItems - product.quantity,
-          totalPrice:
-            state.totalPrice - product.productPrice * product.quantity,
-        }));
+        set((state) => {
+          const cartItem = state.Products.find(
+            (item) => item.id === product.id
+          );
+          if (cartItem) {
+            return {
+              Products: state.Products.filter((item) => item.id !== product.id),
+              totalItems: state.totalItems - cartItem.quantity,
+              totalPrice:
+                state.totalPrice - cartItem.productPrice * cartItem.quantity,
+            };
+          }
+          return state;
+        });
       },
       removeAllProducts: () => {
         set(() => ({
@@ -52,36 +78,54 @@ export const useCart = create<UseCart>()(
         }));
       },
       addQuantity: (product) => {
-        const cart = get().Products;
-        const cartItem = cart.find((item) => item.id === product.id);
-        if (cartItem) {
-          const updatedCart = cart.map((item) =>
-            item.id === product.id
-              ? { ...item, quantity: (item.quantity as number) + 1 }
-              : item
-          );
-          set((state) => ({
-            Products: updatedCart,
-            totalPrice: state.totalPrice + product.productPrice,
-            totalItems: state.totalItems + 1,
-          }));
-        }
+        set((state) => {
+          const cart = get().Products;
+          const cartItem = cart.find((item) => item.id === product.id);
+          if (cartItem) {
+            const updatedCart = cart.map((item) =>
+              item.id === product.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            );
+            return {
+              Products: updatedCart,
+              totalPrice: state.totalPrice + product.productPrice,
+              totalItems: state.totalItems + 1,
+            };
+          }
+          return state;
+        });
       },
       decrementQuantity: (product) => {
-        const cart = get().Products;
-        const cartItem = cart.find((item) => item.id === product.id);
-        if (cartItem) {
-          const updatedCart = cart.map((item) =>
-            item.id === product.id
-              ? { ...item, quantity: (item.quantity as number) - 1 }
-              : item
-          );
-          set((state) => ({
-            Products: updatedCart,
-            totalPrice: state.totalPrice + product.productPrice,
-            totalItems: state.totalItems - 1,
-          }));
-        }
+        set((state) => {
+          const cart = get().Products;
+          const cartItem = cart.find((item) => item.id === product.id);
+          if (cartItem) {
+            if (cartItem.quantity === 1) {
+              // Remove the product if quantity reaches 0
+              return {
+                Products: state.Products.filter(
+                  (item) => item.id !== product.id
+                ),
+                totalItems: state.totalItems - 1,
+                totalPrice: state.totalPrice - product.productPrice,
+              };
+            } else {
+              // Decrement the quantity if it's greater than 1
+              const updatedCart = cart.map((item) =>
+                item.id === product.id
+                  ? { ...item, quantity: item.quantity - 1 }
+                  : item
+              );
+              return {
+                Products: updatedCart,
+                totalPrice: state.totalPrice - product.productPrice,
+                totalItems: state.totalItems - 1,
+              };
+            }
+          }
+          return state;
+        });
       },
     }),
     {
