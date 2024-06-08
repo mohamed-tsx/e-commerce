1d7e363eb04228ecf1dcb4fa4a53de7dd3486cad
Mohamed <mohammedmohamuud.01@gmail.com>
2024-06-08 23:17:27 +0300

    deleted arrow back

--- Client/src/Pages/Checkout Page/Checkout.tsx
+++ Client/src/Pages/Checkout Page/Checkout.tsx
@@ -4,7 +4,7 @@ const Checkout = () => {
     <div className="flex flex-col items-center justify-center mt-12">
       <p className="flex gap-2 items-center">Cart</p>
       <button className="m-4 flex items-center justify-center gap-3">
-        <BiArrowBack /> Continue Shopping
+        Continue Shopping
       </button>
     </div>
   );
