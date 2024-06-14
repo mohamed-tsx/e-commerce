5606cf97b16175f20496b1c57d85935f18a0cad8
Mohamed <mohammedmohamuud.01@gmail.com>
2024-06-14 15:19:28 +0300

    Added two checkboxes for users to choose which shipping plan to select

--- Client/src/Components/Shipping.tsx
+++ Client/src/Components/Shipping.tsx
@@ -24,6 +24,48 @@ const Shipping = () => {
         <div className="mt-20">
           <p className="uppercase font-semibold">Shipping Method</p>
           <hr className="mt-2" />
+          <fieldset className="space-y-4">
+            <legend className="sr-only">Delivery</legend>
+
+            <div>
+              <label
+                htmlFor="DeliveryStandard"
+                className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-black has-[:checked]:ring-1 has-[:checked]:ring-black"
+              >
+                <p className="text-gray-700">Standard {"(5-7) Days"}</p>
+
+                <p className="text-gray-900">$5.00</p>
+
+                <input
+                  type="radio"
+                  name="DeliveryOption"
+                  value="DeliveryStandard"
+                  id="DeliveryStandard"
+                  className="sr-only"
+                  checked
+                />
+              </label>
+            </div>
+
+            <div>
+              <label
+                htmlFor="DeliveryPriority"
+                className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-black has-[:checked]:ring-1 has-[:checked]:ring-black"
+              >
+                <p className="text-gray-700">Rush {"(2-3) Days"}</p>
+
+                <p className="text-gray-900">$15.00</p>
+
+                <input
+                  type="radio"
+                  name="DeliveryOption"
+                  value="DeliveryPriority"
+                  id="DeliveryPriority"
+                  className="sr-only"
+                />
+              </label>
+            </div>
+          </fieldset>
         </div>
       </div>
     </div>
