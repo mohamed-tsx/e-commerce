01a425aeba522da482d2fd90a01ac7a669817c05
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-02 14:57:38 +0300

    deleted the home page

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
 import Header from "./Components/Header/Header";
+import Dashbaord from "./Pages/Dashboard/Dashbaord";
 
 export default function App() {
   return (
     <div className="px-16 py-6">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Dashbaord />} />
       </Routes>
     </div>
   );
--- Dashboard/src/Components/Header/Header.tsx
+++ Dashboard/src/Components/Header/Header.tsx
@@ -1,12 +1,16 @@
+import { RiMenu4Fill } from "react-icons/ri";
 import Logo from "../../../public/Zylo Logo.png";
 const Header = () => {
   return (
     <div className="flex justify-between items-center">
       <img src={Logo} alt="" className="w-10" />
-      <div>
+      <div className="hidden lg:block">
         <button className="flex justify-center items-center bg-black text-white px-4 py-1 rounded-md">
           Login
         </button>
+      </div>
+      <div className="block lg:hidden">
+        <RiMenu4Fill />
       </div>
     </div>
   );
