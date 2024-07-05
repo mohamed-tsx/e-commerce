95c65b55e5ac7a78223ae4bbcfeb95209a37d0eb
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-05 20:25:30 +0300

    set up state management for user auth

---
+++ Dashboard/src/Hooks/useUser.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface UserType {
+  username: string;
+  email: string;
+  photo: string;
+  role: string;
+  id: string;
+}
+
+type UserStore = {
+  User: UserType | null;
+  saveUserInfo: (userInfo: UserType) => void;
+  logout: () => void;
+};
+
+export const useUser = create<UserStore>()(
+  persist(
+    (set, get) => ({
+      User: null,
+      saveUserInfo: (userInfo: UserType) => {
+        set(() => ({ User: (get().User = userInfo) }));
+      },
+      logout: () => {
+        set(() => ({ User: null }));
+      },
+    }),
+    {
+      name: "user-storage",
+    }
+  )
+);
