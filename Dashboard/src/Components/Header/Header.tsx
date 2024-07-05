5cb964012a5b1310685be953f3de5d467aa19213
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-05 20:31:09 +0300

    Added user in header

--- Dashboard/src/Components/Header/Header.tsx
+++ Dashboard/src/Components/Header/Header.tsx
@@ -1,20 +1,24 @@
-import { RiMenu4Fill } from "react-icons/ri";
 import Logo from "/Zylo Logo.png";
 import { Link } from "react-router-dom";
+import { useUser } from "../../Hooks/useUser";
 const Header = () => {
+  const { User } = useUser();
   return (
     <div className="flex justify-between items-center">
       <img src={Logo} alt="" className="w-10" />
-      <div className="hidden lg:block">
-        <Link
-          to={"/login"}
-          className="flex justify-center items-center bg-black text-white px-4 py-1 rounded-md"
-        >
-          Login
-        </Link>
-      </div>
-      <div className="block lg:hidden">
-        <RiMenu4Fill />
+      <div className="">
+        {User ? (
+          <div>
+            <p>{User.username}</p>
+          </div>
+        ) : (
+          <Link
+            to={"/login"}
+            className="flex justify-center items-center bg-black text-white px-4 py-1 rounded-md"
+          >
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
