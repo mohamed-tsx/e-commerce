b6939826f29c870d857ede445a6ab57bcdeca798
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-08 17:35:55 +0300

    Built thetop of the header

--- Dashboard/src/Pages/Dashboard/Dashbaord.tsx
+++ Dashboard/src/Pages/Dashboard/Dashbaord.tsx
@@ -1,7 +1,20 @@
 const Dashbaord = () => {
   return (
     <div className="">
-      <p>dkhwefhweifhiwehfuwefuhweufwueohfvwjbvubdvhbrubvhrbvurh1</p>
+      <div className="p-6 flex justify-center items-center gap-3">
+        <div className="bg-black p-5 w-80 text-white rounded-md">
+          <p>Total Products</p>
+          <p className="text-2xl font-semibold">$64</p>
+        </div>
+        <div className="bg-black p-5 w-96 text-white rounded-md">
+          <p>Total Earn</p>
+          <p className="text-2xl font-semibold">64</p>
+        </div>
+        <div className="bg-black p-5 w-96 text-white rounded-md">
+          <p>Total Orders</p>
+          <p className="text-2xl font-semibold">64</p>
+        </div>
+      </div>
       <div className=""></div>
     </div>
   );
