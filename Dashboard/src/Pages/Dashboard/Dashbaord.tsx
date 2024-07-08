004de89abe3c9f460635134712921de5ab88816e
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-08 17:12:44 +0300

    Removed unwanted styling

--- Dashboard/src/Pages/Dashboard/Dashbaord.tsx
+++ Dashboard/src/Pages/Dashboard/Dashbaord.tsx
@@ -1,6 +1,6 @@
 const Dashbaord = () => {
   return (
-    <div className="outline-1 outline-red-500">
+    <div className="">
       <p>dkhwefhweifhiwehfuwefuhweufwueohfvwjbvubdvhbrubvhrbvurh1</p>
       <div className=""></div>
     </div>
