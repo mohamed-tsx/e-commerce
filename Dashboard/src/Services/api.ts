2d291fc7d3345660d1ae5ebee1e08792f3d7c204
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-11 22:00:17 +0300

    Made the api reqs for a different folder

---
+++ Dashboard/src/Services/api.ts
@@ -0,0 +1,21 @@
+export const checkIfProductsExists = async () => {
+  const apiUrl = "api/products/allProducts";
+  const res = await fetch(apiUrl);
+  const data = await res.json();
+  if (data.success) {
+    const products = data.allProducts;
+    return products.length < 1;
+  }
+  return false;
+};
+
+export const Products = async () => {
+  const apiUrl = "api/products/allProducts";
+  const res = await fetch(apiUrl);
+  const data = await res.json();
+  if (data.success) {
+    const products = data.allProducts;
+    return products;
+  }
+  return `Failed to fetch products ${data.message}`;
+};
