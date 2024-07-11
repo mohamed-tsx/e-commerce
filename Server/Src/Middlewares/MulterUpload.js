f8358fb1660c68c6251898fa8587717102d2529d
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-11 19:23:12 +0300

    create multer middleware

---
+++ Server/Src/Middlewares/MulterUpload.js
@@ -0,0 +1,14 @@
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+
+const multerUpload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+module.export = {
+  multerUpload,
+};
