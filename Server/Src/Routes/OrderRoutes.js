eb33cc1c2e1d8c649d8e9e837b8b973239b6a399
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-12 20:44:16 +0300

    Added all order controller in order route

--- Server/Src/Routes/OrderRoutes.js
+++ Server/Src/Routes/OrderRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { createOrder } = require("../Controllers/OrderController");
+const { createOrder, getAllOrders } = require("../Controllers/OrderController");
+const Verify = require("../Utils/Verify");
+const { isAdmin } = require("../Middlewares/userAuthMiddleware");
 const router = express.Router();
 
-router.post("/create", createOrder);
-
+router.post("/create", Verify, isAdmin, createOrder);
+router.get("/all-orders", Verify, isAdmin, getAllOrders);
 module.exports = router;
