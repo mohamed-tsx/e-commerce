382d1eca3e8bc89373b15cd65bcf7aac68f189aa
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-13 19:09:57 +0300

    Simple change

--- Dashboard/src/Components/Dashboard Sidebar/SideBar.tsx
+++ Dashboard/src/Components/Dashboard Sidebar/SideBar.tsx
@@ -6,7 +6,8 @@ import { MdOutlineAnalytics } from "react-icons/md";
 import { CiSettings } from "react-icons/ci";
 
 const Sidebar = () => {
-  const location = useLocation();2
+  const location = useLocation();
+  2;
   return (
     <div className="fixed inset-0 bg-gray-900 bg-opacity-0 flex justify-start z-50 w-fit">
       <div className="bg-gray-100 h-full w-64 flex flex-col justify-between">
@@ -27,10 +28,10 @@ const Sidebar = () => {
                   to="/"
                   className="flex items-center space-x-2 p-2 rounded"
                 >
-                  <span className="text-xl">
+                  <span className="text-sm">
                     <FiHome />
                   </span>
-                  <span className="text-sm">Home</span>
+                  <span className="text-xs">Home</span>
                 </Link>
               </li>
               <li
@@ -42,10 +43,10 @@ const Sidebar = () => {
                   to="/products"
                   className="flex items-center space-x-2 p-2 rounded"
                 >
-                  <span className="text-xl">
+                  <span className="text-sm">
                     <IoMdPricetag />
                   </span>
-                  <span className="text-sm">Products</span>
+                  <span className="text-xs">Products</span>
                 </Link>
               </li>
               <li
@@ -57,10 +58,10 @@ const Sidebar = () => {
                   to="/orders"
                   className="flex items-center space-x-2 p-2 rounded"
                 >
-                  <span className="text-xl">
+                  <span className="text-sm">
                     <LuArchive />
                   </span>
-                  <span className="text-sm">Orders</span>
+                  <span className="text-xs">Orders</span>
                 </Link>
               </li>
               <li
@@ -72,10 +73,10 @@ const Sidebar = () => {
                   to="/analysis"
                   className="flex items-center space-x-2 p-2 rounded"
                 >
-                  <span className="text-xl">
+                  <span className="text-sm">
                     <MdOutlineAnalytics />
                   </span>
-                  <span className="text-sm">Analysis</span>
+                  <span className="text-xs">Analysis</span>
                 </Link>
               </li>
             </ul>
@@ -88,10 +89,10 @@ const Sidebar = () => {
               location.pathname === "/settings" ? "bg-gray-300" : ""
             }`}
           >
-            <span className="text-xl">
+            <span className="text-sm">
               <CiSettings />
             </span>
-            <span className="text-sm">Settings</span>
+            <span className="text-xs">Settings</span>
           </Link>
         </div>
       </div>
