504b9837f95b8c75ad261171ff290f5a9fa4ac56
Mohamed <mohammedmohamuud.01@gmail.com>
2024-07-15 15:06:27 +0300

    fixed some styling errors in cartProduct component

--- Client/src/Components/cartProduct.tsx
+++ Client/src/Components/cartProduct.tsx
@@ -18,7 +18,7 @@ const CartProduct = ({
   productPrice,
   quantity,
 }: ProductCartProps) => {
-  const removerProduct = useCart((state) => state.removeProduct);
+  const removeProduct = useCart((state) => state.removeProduct);
   const addQuantity = useCart((state) => state.addQuantity);
   const decrementQuantity = useCart((state) => state.decrementQuantity);
   const currentProduct = {
@@ -29,16 +29,19 @@ const CartProduct = ({
     productPrice,
     quantity,
   };
+
   return (
-    <div className="flex items-center border p-4 my-2 rounded">
+    <div className="flex items-center border p-4 my-2 rounded truncate">
       <img
         src={imageUrl}
         alt={productName}
         className="w-24 h-24 object-cover mr-4 rounded"
       />
-      <div className="flex flex-col">
+      <div className="flex flex-col flex-1 ">
         <h3 className="text-lg font-bold">{productName}</h3>
-        <p className="my-1">{productDescription}</p>
+        <p className="my-1" title={productDescription}>
+          {productDescription}
+        </p>
         <p className="text-green-500 font-semibold">
           ${productPrice.toFixed(2)}
         </p>
@@ -51,7 +54,6 @@ const CartProduct = ({
           >
             -
           </button>
-
           <p>{quantity}</p>
           <button
             className="border p-3 rounded-md px-4 font-semibold"
@@ -60,11 +62,9 @@ const CartProduct = ({
             +
           </button>
         </div>
-        <div>
-          <button onClick={() => removerProduct(currentProduct)}>
-            <BiTrash />
-          </button>
-        </div>
+        <button onClick={() => removeProduct(currentProduct)} className="ml-4">
+          <BiTrash />
+        </button>
       </div>
     </div>
   );
