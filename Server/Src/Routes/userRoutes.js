07b7559df997d0551893618d842f5e536694a998
Mohamed <mohammedmohamuud.01@gmail.com>
2024-08-22 20:24:29 +0300

    Added it to user routes

--- Server/Src/Routes/userRoutes.js
+++ Server/Src/Routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { Register, Login } = require("../Controllers/userController");
+const { Register, Login, me } = require("../Controllers/userController");
 const router = express.Router();
+const Verify = require("../Utils/Verify");
 
 router.post("/", Register);
 router.post("/login", Login);
+router.get("/me", Verify, me);
 
 module.exports = router;
