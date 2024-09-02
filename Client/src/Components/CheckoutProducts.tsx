cc3061ab7cc4e7ef94fc672c1e9b91b5b1c1b260
Mohamed <mohammedmohamuud.01@gmail.com>
2024-09-02 17:10:43 +0300

    implemented it frontend

--- Client/src/Components/CheckoutProducts.tsx
+++ Client/src/Components/CheckoutProducts.tsx
@@ -4,8 +4,22 @@ import { useCheckout } from "../Hooks/useCheckout";
 import CheckoutProduct from "./CheckoutProduct";
 
 const CheckoutProducts = () => {
-  const { Products, totalPrice, shippingPrice, setShippingPrice } = useCart();
+  const {
+    Products,
+    totalPrice,
+    shippingPrice,
+    calculateTax,
+    tax,
+    setShippingPrice,
+  } = useCart();
   const { shippingMethod, stage } = useCheckout();
+
+  // Calculate the VAT when the total price changes
+  useEffect(() => {
+    console.log(totalAmount);
+    calculateTax(totalAmountWithTax);
+    // Recalculate VAT whenever the total price changes
+  }, [totalPrice, calculateTax, shippingPrice]);
 
   useEffect(() => {
     if (stage === "Shipping" || stage === "Payment") {
@@ -23,8 +37,10 @@ const CheckoutProducts = () => {
 
   const totalAmount = totalPrice + shippingPrice;
 
+  const totalAmountWithTax = totalAmount + tax;
+
   return (
-    <div className="w-1/2">
+    <div className="w-full md:w-1/2">
       {Products.map((product) => (
         <div key={product.id}>
           <CheckoutProduct {...product} />
@@ -45,10 +61,20 @@ const CheckoutProducts = () => {
           </p>
         </div>
         <div className="flex justify-between">
+          <p>Subtotal: </p>
+          <p>${totalAmount}</p>
+        </div>
+        <div className="flex justify-between">
+          <p>VAT (5%): </p>
+          <p>${tax.toFixed(2)}</p>
+        </div>
+        <div className="flex justify-between">
           <p className="font-medium text-lg">Total: </p>
           <p>
             <span className="text-xs">USD</span>{" "}
-            <span className="font-medium">${totalAmount.toFixed(2)}</span>
+            <span className="font-medium">
+              ${totalAmountWithTax.toFixed(2)}
+            </span>
           </p>
         </div>
       </div>
